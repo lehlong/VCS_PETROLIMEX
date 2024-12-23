6ca4eabe1fddf9f26547587d9029b26171b97bcd
Hải Long <hlonn.it@gmail.com>
2024-12-23 11:39:57 +0700

    Update

--- VCS_WEB/src/app/business/get-goods-display/get-goods-display.component.ts
+++ VCS_WEB/src/app/business/get-goods-display/get-goods-display.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShareModule } from '../../shared/share-module';
 
 @Component({
   selector: 'app-get-goods-display',
   standalone: true,
-  imports: [],
+  imports: [ShareModule],
   templateUrl: './get-goods-display.component.html',
   styleUrl: './get-goods-display.component.scss'
 })
-export class GetGoodsDisplayComponent {
+export class GetGoodsDisplayComponent implements OnInit{
+  isFullscreen: boolean = false;
 
+
+  ngOnInit(){
+this.speechNotify();
+  }
+  speechNotify() : void{
+    const utterance = new SpeechSynthesisUtterance('Xin chào! Đây là bản văn bản chuyển đổi thành giọng nói.')
+    utterance.lang = 'vi-VN';
+    window.speechSynthesis.speak(utterance);
+  }
+  toggleFullscreen(check : boolean) {
+    this.isFullscreen = check;
+  }
 }
