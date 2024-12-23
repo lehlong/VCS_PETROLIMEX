3fc1d39168f2d971236d1c7d9164c0872f9b1ed9
Hải Long <hlonn.it@gmail.com>
2024-12-23 11:13:15 +0700

    Update

--- VCS_WEB/src/app/business/order-display/order-display.component.ts
+++ VCS_WEB/src/app/business/order-display/order-display.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ShareModule } from '../../shared/share-module';
 
 @Component({
   selector: 'app-order-display',
   standalone: true,
-  imports: [],
+  imports: [ShareModule],
   templateUrl: './order-display.component.html',
   styleUrl: './order-display.component.scss'
 })
 export class OrderDisplayComponent {
-
+  isFullscreen: boolean = false;
+  toggleFullscreen(check : boolean) {
+    this.isFullscreen = check;
+  }
 }
