1b1ff7d218f10978c534563aadab44271a4b383f
Hải Long <hlonn.it@gmail.com>
2025-02-06 17:06:17 +0700

    Create NoHoverButton.cs

---
+++ VCS_API/VCS.APP/Utilities/NoHoverButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class NoHoverButton : Button
+{
+    protected override void OnMouseEnter(EventArgs e)
+    {
+        // Do nothing to prevent hover effect
+    }
+
+    protected override void OnMouseLeave(EventArgs e)
+    {
+        // Do nothing to prevent hover effect
+    }
+}
