ad4e5279e94488416a4218b37cbd862e483e8775
Xhuynh16 <hoahuynhln12@gmail.com>
2025-02-07 13:40:25 +0700

    Update

--- VCS_API/VCS.APP/Utilities/Global.cs
+++ VCS_API/VCS.APP/Utilities/Global.cs
@@ -12,7 +12,7 @@ namespace VCS.APP.Utilities
         public static string SmoApiPassword = "smoapi123";
         public static string SmoApiUrl = "https://smoapiuat.petrolimex.com.vn/api/";
         public static string PathSaveFile = "D://AttachImageVCS";
-        public static string Connection = "Server=192.168.110.69;Database=VCS_PETROLIMEX;User ID=sa;Password=sa@d2s.com.vn; TrustServerCertificate=true; MultipleActiveResultSets=true";
+        public static string Connection = "Server=192.168.110.69,1608;Database=VCS_PETROLIMEX;User ID=sa;Password=sa@d2s.com.vn; TrustServerCertificate=true; MultipleActiveResultSets=true";
         public static string DetectApiUrl = "http://localhost:5000/api/detect";
     }
 }
