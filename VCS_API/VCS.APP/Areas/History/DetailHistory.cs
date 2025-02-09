c23e2b2f2eabb6d8beb6b15bb00cad8794a2e7c6
Hải Long <hlonn.it@gmail.com>
2025-02-09 21:57:00 +0700

    Update

---
+++ VCS_API/VCS.APP/Areas/History/DetailHistory.cs
@@ -0,0 +1,30 @@
+﻿using DMS.CORE;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VCS.APP.Areas.History
+{
+    public partial class DetailHistory : Form
+    {
+        private AppDbContext _dbContext;
+        private string _headerId;
+        public DetailHistory(AppDbContext dbContext, string headerId)
+        {
+            InitializeComponent();
+            _dbContext = dbContext;
+            _headerId = headerId;
+        }
+
+        private void DetailHistory_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
