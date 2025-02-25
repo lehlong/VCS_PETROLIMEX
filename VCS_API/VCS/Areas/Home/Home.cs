dad07e48e4796d119c0a384e82b5c6f238386127
Hải Long <hlonn.it@gmail.com>
2025-02-26 00:05:13 +0700

    Update Home.cs

--- VCS_API/VCS/Areas/Home/Home.cs
+++ VCS_API/VCS/Areas/Home/Home.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using DMS.CORE;
+﻿using DMS.CORE;
 using DMS.CORE.Entities.MD;
+using DocumentFormat.OpenXml.Office2010.PowerPoint;
 using LibVLCSharp.Shared;
 using LibVLCSharp.WinForms;
-using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 using VCS.APP.Utilities;
+using Media = LibVLCSharp.Shared.Media;
 
 namespace VCS.Areas.Home
 {
@@ -24,36 +29,36 @@ namespace VCS.Areas.Home
             _dbContext = dbContext;
             InitializeLibVLC();
         }
-
         private void Home_Load(object sender, EventArgs e)
         {
-            Task.Run(InitializeCameraStreams);
+            InitializeCameraStreams();
         }
 
         private void InitializeLibVLC()
         {
             Core.Initialize();
             _libVLC = new LibVLC(
-                "--network-caching=300",
-                "--live-caching=300",
-                "--file-caching=300",
+                "--network-caching=100",
+                "--live-caching=100",
+                "--file-caching=100",
                 "--clock-jitter=0",
                 "--clock-synchro=0",
                 "--no-audio",
-                "--rtsp-tcp",
-                "--h264-hw-decoding"
+                "--rtsp-tcp"
             );
         }
-
-        private async Task InitializeCameraStreams()
+        private async void InitializeCameraStreams()
         {
             try
             {
-                var cameras = await _dbContext.TblMdCamera
-                    .Where(x => x.OrgCode == ProfileUtilities.User.OrganizeCode && x.WarehouseCode == ProfileUtilities.User.WarehouseCode)
-                    .ToListAsync();
+                var cameras = await Task.Run(() =>
+                    _dbContext.TblMdCamera
+                        .Where(x => x.OrgCode == ProfileUtilities.User.OrganizeCode
+                                 && x.WarehouseCode == ProfileUtilities.User.WarehouseCode)
+                        .ToList()
+                );
 
-                Invoke((MethodInvoker)delegate
+                this.Invoke((MethodInvoker)delegate
                 {
                     foreach (var camera in cameras)
                     {
@@ -63,12 +68,19 @@ namespace VCS.Areas.Home
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Lỗi khi tải danh sách camera: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Lỗi khi tải danh sách camera: {ex.Message}", "Lỗi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void AddCameraStream(TblMdCamera camera)
         {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() => AddCameraStream(camera)));
+                return;
+            }
+
             var cameraContainer = new Panel
             {
                 Width = 640,
@@ -78,17 +90,45 @@ namespace VCS.Areas.Home
                 BackColor = Color.FromArgb(52, 58, 64)
             };
 
-            var videoView = new VideoView { Dock = DockStyle.Fill };
-            var media = new Media(_libVLC, camera.Rtsp, FromType.FromLocation);
+            var plateText = camera.IsRecognition ? "(Camera nhận diện)" : "";
+            var label = new Label
+            {
+                Text = camera.IsIn ? $"{camera.Name} - CAMERA CỔNG VÀO {plateText}" : $"{camera.Name} - CAMERA CỔNG RA {plateText}",
+                Dock = DockStyle.Top,
+                Height = 30,
+                ForeColor = Color.White,
+                BackColor = Color.FromArgb(52, 58, 64),
+                Font = new Font("Segoe UI", 12, FontStyle.Regular),
+                TextAlign = ContentAlignment.MiddleCenter
+            };
+
+            cameraContainer.Controls.Add(label);
+
+            var videoView = new VideoView
+            {
+                Dock = DockStyle.Fill
+            };
+
+            string rtspUrl = camera.Rtsp;
+            var media = new Media(_libVLC, rtspUrl, FromType.FromLocation);
             var player = new MediaPlayer(media);
 
             videoView.MediaPlayer = player;
             _mediaPlayers[camera.Code] = player;
+
             cameraContainer.Controls.Add(videoView);
-            (camera.IsIn ? cameraPanelIn : cameraPanelOut).Controls.Add(cameraContainer);
+
+            if (camera.IsIn)
+            {
+                cameraPanelIn.Controls.Add(cameraContainer);
+            }
+            else
+            {
+                cameraPanelOut.Controls.Add(cameraContainer);
+            }
+
             player.Play();
         }
-
         protected override void OnFormClosing(FormClosingEventArgs e)
         {
             foreach (var player in _mediaPlayers.Values)
@@ -97,10 +137,12 @@ namespace VCS.Areas.Home
                     player.Stop();
                 player.Dispose();
             }
+
             _mediaPlayers.Clear();
             cameraPanelIn.Controls.Clear();
             cameraPanelOut.Controls.Clear();
             _libVLC?.Dispose();
+
             base.OnFormClosing(e);
         }
     }
