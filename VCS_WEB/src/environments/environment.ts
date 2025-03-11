a926590a134e29e326a32256d2312d7e5de2af7b
Hải Long <hlonn.it@gmail.com>
2025-03-11 10:28:41 +0700

    Update

--- VCS_WEB/src/environments/environment.ts
+++ VCS_WEB/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //baseApiUrl: 'https://localhost:4008/api',
-  baseApiUrl: 'http://10.6.8.16:1001/api',
+  baseApiUrl: 'https://localhost:4008/api',
   apiUrl: 'apiUrl'
 };
